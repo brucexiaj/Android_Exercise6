0193ff9384af787b3123eac27e80444b9c49d038
brucexiaj <brucexiajun@126.com>
2018-09-17 21:09:26 +0800

    添加第一次作业的代码

---
+++ app/src/main/java/es/source/code/activity/SCOSEntry.java
@@ -0,0 +1,15 @@
+package es.source.code.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import es.source.code.R;
+
+public class SCOSEntry extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.entry);
+    }
+}
